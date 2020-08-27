fa9c0fec571c3b59d3bd925c4a160c5f63574cd8
DmitryBakunovich <dv.bakunovich@gmail.com>
2020-08-27 16:29:41 +0300

    Added registration serializers and views, changed urls

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'api'
+
+urlpatterns = [
+    path('registration/', views.UserRegistration.as_view(), name='registration')
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import *
+
+
+class UserRegistration(APIView):
+
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            data = {
+                'response': 'User created successfully.',
+                'username': user.username,
+                'email': user.email,
+            }
+        else:
+            data = serializer.errors
+        return Response(data)
