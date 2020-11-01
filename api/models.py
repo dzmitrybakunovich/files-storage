ecf8cf517547e566c1369f32273bbbe9e4d9538c
Dmitry Bakunovich <dv.bakunovich@gmail.com>
2020-11-01 21:09:26 +0300

    Added Folder model

--- api/models.py
+++ api/models.py
@@ -15,3 +15,19 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Folder(models.Model):
+    folder_path = models.CharField(max_length=400, null=False, default=None)
+    name = models.CharField(max_length=40, null=False, default=None)
+    created = models.DateTimeField(auto_now_add=True)
+    lastupdated = models.DateTimeField(auto_now=True)
+    owner = models.OneToOneField('api.CustomUser', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.folder_path
+
+
+class LastOpenedToUser(models.Model):
+    user = models.OneToOneField('api.CustomUser', on_delete=models.CASCADE)
+    lastopened = models.DateTimeField(auto_now=True)
